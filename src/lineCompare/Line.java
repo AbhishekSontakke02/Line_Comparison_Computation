d7829337799341cd2e8dd44390823dadbd0e9cfa
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-08-06 11:15:24 +0530

    added: used compareTo to compare two line are equal or not

--- src/lineCompare/Line.java
+++ src/lineCompare/Line.java
@@ -4,24 +4,42 @@ import java.util.Scanner;
 
 public class Line {
 	static Scanner sc = new Scanner(System.in);
-	static double x1,y1,x2,y2;
+	static double x1,y1,x2,y2,x3,y3,x4,y4;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparision Computation Program!");
 		input();
-		double result =calculateLength();
-		System.out.println("Length of Line : "+ result);
+		
+		double line1 =calculateLength(x1,y1,x2,y2);
+		System.out.println("Length of Line : "+ line1);
+		double line2 =calculateLength(x3,y3,x4,y4);
+		System.out.println("Length of Line : "+ line2);
+		
+		if(Double.compare(line1, line2) == 0) {
+			System.out.println("The Two Lines are Equal!");
+		}
+		else {
+			System.out.println("The Two Lines are not Equal!");
+		}
 	}
 	
 	private static void input() {
 		System.out.println("Enter coordinates of the first point (x1, y1): ");
 		x1 = sc.nextDouble();
 		y1 = sc.nextDouble();
-		System.out.println("Enter coordinates of the first point (x1, y1): ");
+		System.out.println("Enter coordinates of the first point (x2, y2): ");
 		x2 = sc.nextDouble();
 		y2 = sc.nextDouble(); 
+
+		System.out.println("Enter coordinates of the first point (x3, y3): ");
+		x3 = sc.nextDouble();
+		y3 = sc.nextDouble();
+		System.out.println("Enter coordinates of the first point (x4, y4): ");
+		x4 = sc.nextDouble();
+		y4 = sc.nextDouble(); 
+		
 	}
 	
-	private static double calculateLength() {
+	private static double calculateLength(double x1, double y1, double x2, double y2) {
 		return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
 	}
 	
